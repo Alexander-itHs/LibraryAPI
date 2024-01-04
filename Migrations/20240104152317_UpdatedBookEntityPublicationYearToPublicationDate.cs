6f45879d36a547b5bd95424cbbb89b978f29fa1c
Alexander-itHs <143017808+Alexander-itHs@users.noreply.github.com>
2024-01-04 16:29:10 +0100

    Changed the name of a property belonging to the Book class.

---
+++ Migrations/20240104152317_UpdatedBookEntityPublicationYearToPublicationDate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedBookEntityPublicationYearToPublicationDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PublicationYear",
+                table: "Books",
+                newName: "PublicationDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PublicationDate",
+                table: "Books",
+                newName: "PublicationYear");
+        }
+    }
+}
--- Models/Book.cs
+++ Models/Book.cs
@@ -5,6 +5,6 @@ public class Book
 	public int BookId { get; set; }
 	public string Title { get; set; } = null!;
 	public string? ISBN { get; set; } = null!;
-	public DateOnly PublicationYear { get; set; }
+	public DateOnly PublicationDate { get; set; }
 	public List<Author> Authors { get; } = new();
 }
